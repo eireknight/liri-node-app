506bf9ff0ec989af64cc4c8d5a35f495b7ae10f4
Patrick O'Brien <patricknathanobrien@gmail.com>
2020-04-10 23:52:27 -0400

    Concert and Spotify

--- liri.js
+++ liri.js
@@ -1,36 +1,53 @@
-// Info for Spotify
 require("dotenv").config();
+
 var Spotify = require("node-spotify-api");
+
 var keys = require("./keys.js");
+
 var spotify = new Spotify(keys.spotify);
 
-// Info for Request
 var request = require("request");
 
-// Info for Moment
 var moment = require("moment");
 
-// Info for fs
+var axios = require("axios");
+
 var fs = require("fs");
 
-
-// Command process
 var command = process.argv[2];
 
-
-// Concert command
 if (command === "concert-this") {
 
     var artist = process.argv[3];
 
     request("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp", function (error, response, body) {
 
-        if (!error && response.statusCode === 200) {
             console.log("-------------------------------------");
             console.log("Venue: " + JSON.parse(body)[0].venue.name);
             console.log("Location: " + JSON.parse(body)[0].venue.city + " " + JSON.parse(body)[0].venue.region);
             console.log("Date: " + moment(JSON.parse(body)[0].datetime).format("MM/DD/YYYY"));
             console.log("-------------------------------------");
-        }
     });
 
+} else if (command === "spotify-this-song") {
+    var song = process.argv[3];
+
+    if (song === undefined) {
+        song = "The Sign";
+    }
+
+    spotify.search({
+        type: "track",
+        query: song,
+        limit: 1
+    }, function (err, data) {
+        if (err) {
+            return console.log("Error occured: " + err);
+        }
+        console.log("-------------------------------------");
+        console.log("Artist: " + data.tracks.items[0].artists[0].name);
+        console.log("Song Name: " + data.tracks.items[0].name);
+        console.log("Preview Link: " + data.tracks.items[0].preview_url);
+        console.log("Album: " + data.tracks.items[0].album.name);
+        console.log("-------------------------------------");
+    });
